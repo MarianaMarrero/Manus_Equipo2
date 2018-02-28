b147d3e8961ab417e96ef1dab8d8155f1df4dafb
MarianaMarrero <marimarr22@gmail.com>
2018-02-28 15:33:32 -0300

    probando

---
+++ WindowsFormsManus/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsManus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+      
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCargarImagen = new System.Windows.Forms.Button();
+            this.picImagen = new System.Windows.Forms.PictureBox();
+            this.btnConvertirImagen = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCargarImagen
+            // 
+            this.btnCargarImagen.Location = new System.Drawing.Point(12, 515);
+            this.btnCargarImagen.Name = "btnCargarImagen";
+            this.btnCargarImagen.Size = new System.Drawing.Size(113, 28);
+            this.btnCargarImagen.TabIndex = 1;
+            this.btnCargarImagen.Text = "Cargar imagen";
+            this.btnCargarImagen.UseVisualStyleBackColor = true;
+            this.btnCargarImagen.Click += new System.EventHandler(this.btnCargarImagen_Click);
+            // 
+            // picImagen
+            // 
+            this.picImagen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picImagen.BackColor = System.Drawing.Color.White;
+            this.picImagen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picImagen.Location = new System.Drawing.Point(12, 12);
+            this.picImagen.Name = "picImagen";
+            this.picImagen.Size = new System.Drawing.Size(532, 497);
+            this.picImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImagen.TabIndex = 0;
+            this.picImagen.TabStop = false;
+            // 
+            // btnConvertirImagen
+            // 
+            this.btnConvertirImagen.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnConvertirImagen.Location = new System.Drawing.Point(992, 517);
+            this.btnConvertirImagen.Name = "btnConvertirImagen";
+            this.btnConvertirImagen.Size = new System.Drawing.Size(113, 27);
+            this.btnConvertirImagen.TabIndex = 3;
+            this.btnConvertirImagen.Text = "Convertir imagen";
+            this.btnConvertirImagen.UseVisualStyleBackColor = true;
+            this.btnConvertirImagen.Click += new System.EventHandler(this.btnConvertirImagen_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResultado.Location = new System.Drawing.Point(550, 12);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(555, 497);
+            this.txtResultado.TabIndex = 2;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lblInfo.Location = new System.Drawing.Point(131, 523);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(10, 13);
+            this.lblInfo.TabIndex = 7;
+            this.lblInfo.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 566);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnConvertirImagen);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnCargarImagen);
+            this.Controls.Add(this.picImagen);
+            this.Name = "Form1";
+            this.Text = "Manus";
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargarImagen;
+        private System.Windows.Forms.PictureBox picImagen;
+        private System.Windows.Forms.Button btnConvertirImagen;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ WindowsFormsManus/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows.Forms;
+using Google.Cloud.Vision.V1;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using ImageProcessor.Imaging.Formats;
+using ImageProcessor;
+using System.Drawing.Imaging;
+using ImageProcessor.Imaging;
+using ImageProcessor.Imaging.Filters.Photo;
+
+namespace WindowsFormsManus
+{
+
+
+    public partial class Form1 : Form 
+    {
+        public string ImagePath { get; set; }
+        OpenFileDialog fileDlg = new OpenFileDialog();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnConvertirImagen.Enabled = false;
+        }
+
+        //**Método que carga la imagen a convertir**//
+        private void btnCargarImagen_Click(object sender, EventArgs e)
+        {
+            btnConvertirImagen.Enabled = false;
+            txtResultado.Text = "";
+            ImagePath = "";
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+               dlg.Filter = "jpeg files|*.jpg;*.JPG;*.png;*.PNG;";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo fileInfo = new FileInfo(dlg.FileName);
+                    if (fileInfo.Length > 1024 * 1024 * 4)
+                    {
+                        MessageBox.Show("El tamaño de la imagen no puede ser mayor a 4MB");
+                        return;
+                    }
+                    else
+                    {
+                        picImagen.Image = System.Drawing.Image.FromFile(dlg.FileName);
+                        ImagePath = dlg.FileName;
+                        lblInfo.Text = "Imagen cargada: " + fileInfo.Name;
+                        lblInfo.BackColor = Color.LightGreen;
+
+                        btnConvertirImagen.Enabled = true;
+                    }
+                }
+            }         
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //**Método que convierte a texto la imagen a cargada**//
+        private void btnConvertirImagen_Click(object sender, EventArgs e)
+        {
+            
+            string selectedFile = ImagePath;
+            var photoBytes = File.ReadAllBytes(selectedFile);
+          
+                //**Realiza procesamiento de imagen previo a la conversion**//
+                using (var inStream = new MemoryStream(photoBytes))
+                {
+                 using (var outStream = new MemoryStream())
+                 {
+                    using (ImageFactory imageFactory = new ImageFactory(false))
+                    {
+                        //Filtros 
+                        var img = imageFactory.Load(inStream)
+                                 //  .Filter(MatrixFilters.BlackWhite)
+                                 //  .Saturation(5)
+                                   .Brightness(10)
+                                  // .Contrast(5)
+                                   .Format(new PngFormat { Quality = 100 })
+                                   .Resolution(1000, 1000)
+                               //  .Quality(100)
+                               //Se guarda en memoria los cambios
+                               .Save(outStream);
+                    }
+
+                //Realiza la conversion con la imagen a texto a pertir de los cambios realizados  
+                var image = Google.Cloud.Vision.V1.Image.FromStream(outStream);
+                var client = ImageAnnotatorClient.Create();
+                var response = client.DetectDocumentText(image);//MODIFICADO*/
+                txtResultado.Text = response.Text;
+
+                string[] renglones = txtResultado.Text.Split('\n');
+                txtResultado.Text = "";
+                foreach (var renglon in renglones)
+                    {
+                    txtResultado.Text = string.Concat(txtResultado.Text, renglon);
+                    txtResultado.Text = string.Concat(txtResultado.Text, Environment.NewLine);
+                    }
+                  }
+               } 
+           }
+      }
+}
+
